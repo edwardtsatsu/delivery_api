984a6d32067d1ffd4035468095f675c00b4d55d8
edwardtsatsu <edwardakorlie73@gmail.com>
2022-11-30 13:44:58 +0000

    perfom a class clock

--- src/main/controllers/generate_otp_controller.py
+++ src/main/controllers/generate_otp_controller.py
@@ -1,13 +1,13 @@
 from flask import Blueprint
-from flask_pydantic import  validate
+from flask_pydantic import validate
+
 from src.main.requests.generate_otp_request import GenerateOtpRequest
-from src.main.services.generated_otp_service import generate_otp
+from src.main.services.generated_otp_service import GenerateOtpService
 
-
-generate_otp_blueprint = Blueprint('generate-otp', "__name__")
+generate_otp_blueprint = Blueprint("generate-otp", "__name__")
 
 
 @generate_otp_blueprint.route("/user/generate-otp", methods=["POST"])
 @validate()
 def create_otp(body: GenerateOtpRequest):
-    return generate_otp(body)
+    return GenerateOtpService(body).generate_otp()
--- src/main/services/generated_otp_service.py
+++ src/main/services/generated_otp_service.py
@@ -8,42 +8,48 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from src.extensions import db
-from src.main.requests.generate_otp_request import GenerateOtpRequest
-from src.main.responses.generate_otp_response import GenerateOtpResponse, OtpErrorResponse
+from src.main.responses.generate_otp_response import (
+    GenerateOtpResponse,
+    OtpErrorResponse,
+)
 
 from ..models.otp_code_model import OtpCode
 
 
-def generate_otp(body: GenerateOtpRequest):
-    otp_code = randint(1000, 9999)
-    payload = sms_payload(otp_code, body)
+class GenerateOtpService:
+    def __init__(self, body):
+        self.phone_number = body.phone_number
 
-    response = requests.post(url=os.getenv("SMS_URL"), json=payload)
+    def generate_otp(self):
+        self.otp_code = randint(1000, 9999)
+        payload = self.sms_payload()
 
-    # if sent the succesfuly, save code in db
-    if response.json()["responseCode"] == 200:
-        otp = OtpCode(
-            code=otp_code,
-            phone_number=body.phone_number,
-            generated_at=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        )
-        save_otp_code(otp)
-        return GenerateOtpResponse()
-    else:
-        return OtpErrorResponse()
+        response = requests.post(url=os.getenv("SMS_URL"), json=payload)
 
+        print(response.json())
 
-def sms_payload(otp, body):
-    return {
-        "username": os.getenv("USER_NAME"),
-        "password": os.getenv("PASSWORD"),
-        "senderId": os.getenv("SENDER_ID"),
-        "messageType": int(os.getenv("MESSAGE_TYPE")),
-        "msisdn": body.phone_number,
-        "message": os.getenv("MESSAGE") + str(otp),
-    }
+        # if sent the succesfuly, save code in db
+        if response.json()["responseCode"] == 200:
+            self.otp = OtpCode(
+                code=self.otp_code,
+                phone_number=self.phone_number,
+                generated_at=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            )
+            self.save_otp_code()
+            return GenerateOtpResponse()
+        else:
+            return OtpErrorResponse()
 
+    def sms_payload(self):
+        return {
+            "username": os.getenv("USER_NAME"),
+            "password": os.getenv("PASSWORD"),
+            "senderId": os.getenv("SENDER_ID"),
+            "messageType": int(os.getenv("MESSAGE_TYPE")),
+            "msisdn": self.phone_number,
+            "message": os.getenv("MESSAGE") + str(self.otp_code),
+        }
 
-def save_otp_code(otp_obj):
-    db.session.add(otp_obj)
-    db.session.commit()
+    def save_otp_code(self):
+        db.session.add(self.otp)
+        db.session.commit()
