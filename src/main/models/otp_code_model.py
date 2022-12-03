ce6d24c26f8b35e07adb7c40e4d1e049620f10a0
edwardtsatsu <edwardakorlie73@gmail.com>
2022-12-03 17:41:29 +0000

    added index to tables

--- src/main/models/otp_code_model.py
+++ src/main/models/otp_code_model.py
@@ -9,8 +9,10 @@ class OtpCode(db.Model):
 
     __tablename__ = "otp_codes"
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    code = db.Column(db.String(80), unique=False, nullable=False)
-    verified = db.Column(db.Boolean, unique=False, nullable=False, default=False)
-    phone_number = db.Column(db.String, unique=False, nullable=False)
-    generated_at = db.Column(db.DateTime, unique=False, nullable=False)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    code = db.Column(db.String(80), unique=False, nullable=False, index=True)
+    verified = db.Column(
+        db.Boolean, unique=False, nullable=False, default=False, index=True
+    )
+    phone_number = db.Column(db.String, unique=False, nullable=False, index=True)
+    generated_at = db.Column(db.DateTime, unique=False, nullable=False, index=True)
--- src/main/models/user_model.py
+++ src/main/models/user_model.py
@@ -8,8 +8,8 @@ from src.extensions import db
 association_table = db.Table(
     "user_roles",
     db.metadata,
-    db.Column("user_id", db.ForeignKey("users.id"), primary_key=True),
-    db.Column("role_id", db.ForeignKey("roles.id"), primary_key=True),
+    db.Column("user_id", db.ForeignKey("users.id"), primary_key=True,index=True),
+    db.Column("role_id", db.ForeignKey("roles.id"), primary_key=True,index=True),
 )
 
 
@@ -18,15 +18,16 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    phone_number = db.Column(db.String(100), unique=True, nullable=False)
-    username = db.Column(db.String(50), unique=False)
-    password = db.Column(db.String(255), nullable=False)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,index=True)
+    email = db.Column(db.String(255), unique=True, nullable=False,index=True)
+    phone_number = db.Column(db.String(100), unique=True, nullable=False,index=True)
+    username = db.Column(db.String(50), unique=False,index=True)
+    password = db.Column(db.String(255), nullable=False,index=True)
     created_at = db.Column(
         db.DateTime,
         nullable=False,
         default=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        index=True,
     )
     roles = db.relationship("Role", secondary=association_table, back_populates="users")
 
@@ -35,6 +36,6 @@ class Role(db.Model):
 
     __tablename__ = "roles"
 
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    role_name = db.Column(db.String(100), unique=True, nullable=False)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,index=True)
+    role_name = db.Column(db.String(100), unique=True, nullable=False,index=True)
     users = db.relationship("User", secondary=association_table, back_populates="roles")
